be9d9563e36dcb4e8e3478fdc159781a6bcb09d0
GiselleBarbosa <g88barbosa@gmail.com>
2024-10-30 08:40:48 -0300

    feat: finaliza implementação do controller

--- src/main/java/com/barbosa/loans/controller/LoanController.java
+++ src/main/java/com/barbosa/loans/controller/LoanController.java
@@ -1,13 +1,27 @@
 package com.barbosa.loans.controller;
 
+import com.barbosa.loans.controller.dto.CustomerLoanRequest;
+import com.barbosa.loans.controller.dto.CustomerLoanResponse;
+import com.barbosa.loans.services.LoanService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoanController {
 
+    private final LoanService loanService;
+
+    public LoanController(LoanService loanService) {
+        this.loanService = loanService;
+    }
+
     @PostMapping(value = "customer-loans")
-    public ResponseEntity<?> customerLoans() {
+    public ResponseEntity<CustomerLoanResponse> customerLoans(@RequestBody CustomerLoanRequest request) {
+
+        var loanResponse = loanService.checkLoanAvailability(request);
+        return ResponseEntity.ok(loanResponse);
     }
 }
